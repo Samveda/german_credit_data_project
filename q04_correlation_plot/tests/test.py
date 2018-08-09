e29c343a57e949c6bfda65a727c7edcbcd879774
Nikhil Nair <nnair25@gmail.com>
2018-08-09 10:23:47 +0530

    Update test.py

--- q04_correlation_plot/tests/test.py
+++ q04_correlation_plot/tests/test.py
@@ -4,7 +4,7 @@ from greyatomlib.statlog_german_credit_data_project.q03_encode_features.build im
 from greyatomlib.statlog_german_credit_data_project.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 from inspect import getfullargspec
 
-path = "./data/GermanData.csv"
+path = "data/GermanData.csv"
 data=q01_load_data_and_add_column_names(path)
 data=q03_encode_features(data)
 q04_correlation_plot(data)
